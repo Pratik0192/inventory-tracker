94ef91d5acaf1303934eda05acb60bee4d08849f
Pratik0192 <pratikchakraborty019@gmail.com>
2025-07-26 01:00:57 +0530

    updated

--- src/app/common/page.tsx
+++ src/app/common/page.tsx
@@ -3,8 +3,8 @@
 export default function CommonDashboard() {
   return ( 
     <div>
-      <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
-      <p>Welcome to the Dashboard.</p>
+      <h1 className="text-2xl text-primary font-semibold mb-4">Dashboard</h1>
+      <p className="text-foreground">Welcome to the Dashboard.</p>
     </div>
   )
 }
--- src/components/layout/CommonNavbar.tsx
+++ src/components/layout/CommonNavbar.tsx
@@ -4,8 +4,8 @@ import { ModeToggle } from "../themeToggle";
 
 export default function CommonNavbar() {
   return (
-    <header className="w-full h-16 px-6 flex items-center justify-between border-b shadow-sm bg-background-light dark:bg-background-dark text-gray-900 dark:text-gray-100">
-      <div className="text-lg font-semibold">Welcome</div>
+    <header className="w-full h-16 px-6 flex items-center justify-between border-b shadow-sm bg-background text-foreground">
+      <div className="text-primary">Welcome</div>
       <ModeToggle />
     </header>
   )
